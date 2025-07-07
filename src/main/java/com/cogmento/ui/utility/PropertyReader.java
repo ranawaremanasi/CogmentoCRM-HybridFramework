e95e8fad3a9a3472704d10317a3a060a71700914
ranawaremanasi <97731232+ManasiRanaware-BenchmarkitSolutions@users.noreply.github.com>
2025-07-07 17:35:17 +0530

    Driver Factory added to framework

--- src/main/java/com/cogmento/ui/utility/PropertyReader.java
+++ src/main/java/com/cogmento/ui/utility/PropertyReader.java
@@ -1,5 +1,22 @@
 package com.cogmento.ui.utility;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class PropertyReader {
 
+	public static String getProprty(String keyName) {
+		String path = System.getProperty("user.dir") + "/src/main/java/com/cogmento/ui/configuration/config.properties";
+		FileInputStream file;
+		Properties prop = null;
+		try {
+			file = new FileInputStream(path);
+			prop = new Properties();
+			prop.load(file);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return PropertyReader.getProprty(keyName);
+	}
 }
