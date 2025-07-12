c5dc3f431693ab8a8edc366e0163b16f708f8b3b
ranawaremanasi <rmanasi17@gmail.com>
2025-07-13 01:12:38 +0530

    Calendar class implemented

--- src/main/java/com/cogmento/ui/iHelper/ICalendar.java
+++ src/main/java/com/cogmento/ui/iHelper/ICalendar.java
@@ -1,6 +1,12 @@
 package com.cogmento.ui.iHelper;
 
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+
 public interface ICalendar {
 
-	
+	void selectMonthAndYear(WebElement webElementMonthAndYear, WebElement nextBtn, String expectedMonthANdYear);
+
+	void selectDate(List<WebElement> list, String expectedDate);
 }
--- src/main/java/com/cogmento/ui/utility/CalendarImpl.java
+++ src/main/java/com/cogmento/ui/utility/CalendarImpl.java
@@ -1,7 +1,42 @@
 package com.cogmento.ui.utility;
 
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+
+import com.cogmento.ui.base.Base;
 import com.cogmento.ui.iHelper.ICalendar;
 
-public class CalendarImpl implements ICalendar{
+public class CalendarImpl extends Base implements ICalendar {
+
+	WaitImpl wait = new WaitImpl();
+	WebElementImpl webElement = new WebElementImpl();
+
+	@Override
+	public void selectMonthAndYear(WebElement webElementMonthAndYear, WebElement nextBtn, String expectedMonthANdYear) {
+
+		while (true) {
+
+			if (webElement.getText(webElementMonthAndYear).contains(expectedMonthANdYear)) {
+
+				break;
+			} else {
+				webElement.click(nextBtn);
+			}
+		}
+	}
+
+	@Override
+	public void selectDate(List<WebElement> list, String expectedDate) {
+
+		for (WebElement selectDate : wait.visiblilityOfAllElements(list)) {
+
+			if (webElement.getText(selectDate).equals(expectedDate)) {
+
+				wait.elementToBeClickable(selectDate).click();
+				break;
+			}
+		}
+	}
 
 }
