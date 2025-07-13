31ad06c3e85209858d2333aa928d6c1b2e131186
ranawaremanasi <rmanasi17@gmail.com>
2025-07-13 16:25:31 +0530

    Listener class added Screenshot class added Window class added

--- src/main/java/com/cogmento/ui/iHelper/IWindowHandle.java
+++ src/main/java/com/cogmento/ui/iHelper/IWindowHandle.java
@@ -1,5 +1,8 @@
 package com.cogmento.ui.iHelper;
 
+import java.util.Set;
+
 public interface IWindowHandle {
 
+	void switchToWindow(Set<String> sets, int windowIndex);
 }
--- src/main/java/com/cogmento/ui/utility/SceenshotImpl.java
+++ src/main/java/com/cogmento/ui/utility/SceenshotImpl.java
@@ -1,19 +1,62 @@
 package com.cogmento.ui.utility;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import com.cogmento.ui.base.Base;
 import com.cogmento.ui.iHelper.IScreenshot;
 
-public class Sceenshot implements IScreenshot {
+import ru.yandex.qatools.ashot.AShot;
+import ru.yandex.qatools.ashot.shooting.ShootingStrategies;
+
+public class SceenshotImpl extends Base implements IScreenshot {
+
+	DateAndTimeImpl dateAndTime = new DateAndTimeImpl();
 
 	@Override
-	public void takeScreenShot() {
-		
-		
+	public String takeScreenShot(String folderName, String testCaseName) {
+
+		String path = System.getProperty("user.dir") + "//Screenshot//" + dateAndTime.getDateAndTime("yyyy") + "//"
+				+ dateAndTime.getDateAndTime("MMMyyyy") + "//" + dateAndTime.getDateAndTime("ddMMMyyyy") + "//"
+				+ folderName + "//" + testCaseName + dateAndTime.getDateAndTime("ddMMMyyyy_HHmmss") + ".png";
+
+		TakesScreenshot takesScreenshot = (TakesScreenshot) getDriver();
+		File source = takesScreenshot.getScreenshotAs(OutputType.FILE);
+		File destination = new File(path);
+
+		try {
+			FileUtils.copyFile(source, destination);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return path;
 	}
 
 	@Override
-	public void fullPageScreenshot() {
-		
-		
+	public String fullPageScreenshot(String folderName, String testCaseName) {
+
+		String path = System.getProperty("user.dir") + "//Screenshot//" + dateAndTime.getDateAndTime("yyyy") + "//"
+				+ dateAndTime.getDateAndTime("MMMyyyy") + "//" + dateAndTime.getDateAndTime("ddMMMyyyy") + "//"
+				+ folderName + "//" + testCaseName + dateAndTime.getDateAndTime("ddMMMyyy_HHmmss") + ".png";
+
+		AShot ashot = new AShot();
+		BufferedImage source = ashot.shootingStrategy(ShootingStrategies.viewportPasting(1000))
+				.takeScreenshot(getDriver()).getImage();
+
+		try {
+			ImageIO.write(source, "PNG", new File(path));
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+		return path;
 	}
 
 }
